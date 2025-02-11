ede770a0ad8d8c2bb3917c2da7825fe9a7dca693
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-11 18:56:16 -0500

    chore: cleanup

--- crates/threads/src/main.rs
+++ crates/threads/src/main.rs
@@ -7,27 +7,12 @@ use std::thread;
 use crate::error::ErrWrapper;
 pub type Result<T> = std::result::Result<T, ErrWrapper>;
 use owo_colors::OwoColorize;
-use tracing as tea;
 
 fn main() {
-        // fn main() -> Result<()> {
-        let start_time = std::time::Instant::now();
-        // let _writer_guard = {
-        //         utilities::activate_global_default_tracing_subscriber()
-        //                 .maybe_env_default_level(None)
-        //                 .maybe_trace_error_level(None)
-        //                 .call()
-        //                 .expect("Failed to set up tracing subscriber.")
-        // };
-
-        for _ in 0..500 {
+        for _ in 0..5 {
                 thread::spawn(f);
         }
         println!("{} from the {} thread.", "Hello".cyan(), "main".blue());
-
-        let total_time_elapsed = start_time.elapsed();
-        tea::info!(?total_time_elapsed);
-        // Ok(())
 }
 
 fn f() {
