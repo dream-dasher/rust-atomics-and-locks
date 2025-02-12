a582379e0fdbb149aad8b44f54933cb3c0ed5967
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-12 13:32:25 -0500

    feat: simple scoped threads example

---
+++ crates/threads/src/bin/simple_scoped_threads.rs
@@ -0,0 +1,26 @@
+//! # Scratch code for [Rust Atomics and Locks](https://marabos.nl/atomics/)
+
+use std::thread;
+
+use owo_colors::OwoColorize;
+
+fn main() {
+        let numbers = [0, 1, 2, 3, 4];
+        thread::scope(|s| {
+                s.spawn(|| {
+                        println!("length: {}", numbers.len().cyan());
+                });
+                s.spawn(|| {
+                        println!("sum: {}", numbers.iter().sum::<i32>().magenta());
+                });
+                s.spawn(|| {
+                        println!(
+                                "product (exclude `0`): {}",
+                                numbers.iter().filter(|&&n| n > 0).product::<i32>().yellow()
+                        );
+                });
+                s.spawn(|| {
+                        println!("The numbers: {:?}", numbers.on_black());
+                });
+        });
+}
