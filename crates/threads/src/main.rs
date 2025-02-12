a4bc4692e8e6d9e75d6b115f8f21d78a2aa0ff5c
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-11 19:10:03 -0500

    feat: clap interface
    note: threads made in one pass of main can still
    finish and print in other iterations.

--- crates/threads/src/main.rs
+++ crates/threads/src/main.rs
@@ -2,17 +2,46 @@
 //!
 
 mod error;
-use std::thread;
 
 use crate::error::ErrWrapper;
 pub type Result<T> = std::result::Result<T, ErrWrapper>;
+
+use std::thread;
+
+use clap::Parser;
 use owo_colors::OwoColorize;
 
+/// interface for scratch code for use with [Rust Atomics and Locks](https://marabos.nl/atomics/)
+#[derive(Parser, Debug)]
+#[command(version, about, long_about, disable_help_subcommand = true, subcommand_help_heading = "input source")]
+struct Args {
+        /// number of threads to spawn
+        #[arg(default_value = "3")]
+        threads: usize,
+        /// wait on threads at end of main
+        #[arg(short, long)]
+        wait_on: bool,
+        /// number of times to repeat main{}
+        #[arg(short, long, default_value = "0")]
+        repeats: usize,
+}
 fn main() {
-        for _ in 0..5 {
-                thread::spawn(f);
+        let args = Args::parse();
+        dbg!(&args);
+        for _ in 0..1 + args.repeats {
+                println!("--------------------------");
+                let mut handles = vec![];
+                for _ in 0..args.threads {
+                        let h = thread::spawn(f);
+                        handles.push(h);
+                }
+                println!("{} from the {} thread.", "Hello".cyan(), "main".blue());
+                if args.wait_on {
+                        for h in handles {
+                                h.join().unwrap();
+                        }
+                }
         }
-        println!("{} from the {} thread.", "Hello".cyan(), "main".blue());
 }
 
 fn f() {
