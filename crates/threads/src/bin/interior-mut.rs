6dc9f2e86c2a17eedb90020d90d794d2773b957a
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-27 22:51:29 -0500

    feat: interior mut, non-concurrent

--- crates/threads/src/bin/interior-mut.rs
+++ crates/threads/src/bin/interior-mut.rs
@@ -8,10 +8,74 @@
 //!   - `UnsafeCell`
 //!     - unsafe
 //!   - `OnceCell`
-//!     - inner immutable except once
+//!     - share-borrow only once init'd; can only be init'd once
+//!     - only *sorta* 'inner mut' -- *usually* it's way to immutably share, but perform an init if needed (which is often acontextual in content, but not in principle).
 //! - Concurrent Cells
 //!   - `Mutex`
 //!   - `RwLock`
 //!
 
-fn main() {}
+fn main() {
+        // Cell
+        {
+                use std::cell::Cell;
+                let cell = Cell::new(0);
+                println!("cell.get() = {}", cell.get());
+                cell.set(1);
+                println!("- cell.set (1) ->");
+                println!("cell.get() = {}", cell.get());
+                let mut holder = 17;
+                println!("holder = {}", holder);
+                holder = cell.replace(holder);
+                println!("- holder = cell.replace(holder) ->");
+                println!("cell.get() = {}", cell.get());
+                println!("holder = {}", holder);
+        }
+        println!("------");
+
+        // RefCell
+        {
+                use std::cell::RefCell;
+                let refcell = RefCell::new(0);
+                println!("refcell.borrow() = {}", refcell.borrow());
+                *refcell.borrow_mut() = 1;
+                println!("- refcell.borrow_mut() = 1 ->");
+                println!("refcell.borrow() = {}", refcell.borrow());
+        }
+        println!("------");
+
+        // UnsafeCell
+        {
+                use std::cell::UnsafeCell;
+                let unsafe_cell = UnsafeCell::new(0);
+                // SAFETY: writing a direct value through an `UnsafeCell`
+                // here is safe because we have exclusive access and know
+                // there are no other references
+                unsafe {
+                        *unsafe_cell.get() = 1;
+                }
+                println!("- unsafe_cell.get() = 1 ->");
+                // `.get()` yields a dereferenced mut value; using `into_inner()` for simplicity here
+                println!("unsafe_cell.into_inner() = {}", unsafe_cell.into_inner());
+        }
+        println!("------");
+
+        // OnceCell
+        {
+                use std::cell::OnceCell;
+                let celluno = OnceCell::new();
+                println!("celluno.get() = {:?}", celluno.get());
+                println!("celluno.get_or_init(|| 1) = {}", celluno.get_or_init(|| 1));
+                println!("celluno.get_or_init(|| 2) = {}", celluno.get_or_init(|| 2));
+                println!("celluno.get_or_init(|| 3) = {}", celluno.get_or_init(|| 3));
+        }
+        println!("------");
+
+        // Mutex
+        {}
+        println!("------");
+
+        // RwLock
+        {}
+        println!("------");
+}
