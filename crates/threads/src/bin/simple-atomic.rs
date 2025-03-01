aedc7fb815279ee429fcbcf39885602864cef05d
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-03-01 18:35:25 -0500

    wip: atomic example

--- crates/threads/src/bin/simple-atomic.rs
+++ crates/threads/src/bin/simple-atomic.rs
@@ -5,7 +5,7 @@
 //! - Fetch_&_Modify
 //! - Compare_&_Exchange
 
-use std::{sync::atomic::{AtomicBool, Ordering::Relaxed},
+use std::{sync::atomic::{AtomicBool, AtomicUsize, Ordering::Relaxed},
           thread};
 
 use owo_colors::OwoColorize as _;
@@ -34,5 +34,32 @@ fn main() {
                 }
                 STOP.store(true, Relaxed);
                 background_thread.join().unwrap();
+        }
+        {
+                println!("\n-----{}-----", "Single extraThread Update Sync".bold().purple());
+                // this should really be a `fetch_add`
+                let atomic_num_done = AtomicUsize::new(0);
+                let main_thread_handle = thread::current(); // for unparking
+                thread::scope(|s| {
+                        // 'background thread' processing 100 items
+                        s.spawn(|| {
+                                for _ in 0..100 {
+                                        thread::sleep(std::time::Duration::from_millis(10)); // fake processing
+                                        let current_done = atomic_num_done.load(Relaxed);
+                                        atomic_num_done.store(current_done + 1, Relaxed);
+                                        main_thread_handle.unpark(); // wake main up
+                                }
+                        });
+                        loop {
+                                let current_done = atomic_num_done.load(Relaxed);
+                                println!("Processed {}/100 items", current_done.blue());
+                                if current_done >= 100 {
+                                        println!("{}", "All items processed".green());
+                                        break;
+                                } else {
+                                        thread::park(); // for efficiency
+                                }
+                        }
+                });
         }
 }
